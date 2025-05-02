869096e81d11aeb6c30a4a74b0aed8005d9f3285
SGZ-06 <175166640+SGZ-06@users.noreply.github.com>
2025-05-02 14:57:41 -0600

    Add the following solved files: index.html, index.css, index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+let homeScore = document.getElementById("home-score");
+let guestScore = document.getElementById("guest-score");
+
+let countHome = 0
+let countGuest = 0
+
+function addHome1() {
+    countHome += 1
+    homeScore.textContent = countHome;   
+}
+
+function addHome2() {
+    countHome += 2
+    homeScore.textContent = countHome;   
+}
+
+function addHome3() {
+    countHome += 3
+    homeScore.textContent = countHome;   
+}
+
+function addGuest1() {
+    countGuest += 1
+    guestScore.textContent = countGuest;   
+}
+
+function addGuest2() {
+    countGuest += 2
+    guestScore.textContent = countGuest;   
+}
+
+function addGuest3() {
+    countGuest += 3
+    guestScore.textContent = countGuest;   
+}
+
+const restartGame = () => {
+    countHome = 0
+    homeScore.textContent = 0;
+    countGuest = 0
+    guestScore.textContent = 0;
+    
+}
+
